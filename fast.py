70135ba8968f5d0fb0aa6977a237d8cb2ad3affc
Tharak Ram <62231625+THARAK-RAM1@users.noreply.github.com>
2021-12-25 16:03:02 +0530

    Update fast.py

--- fast.py
+++ fast.py
@@ -3,7 +3,7 @@ import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world from python tharak3")
+        self.write("Hello, world from python sruthy")
 
 def make_app():
     return tornado.web.Application([
